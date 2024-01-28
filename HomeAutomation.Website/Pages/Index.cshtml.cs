21c00d6bcbe6626a7ba7c2dd1490a8b1c5174ade
bob h <mail@bob.codes>
2024-01-28 20:21:59 +0000

    adding website

---
+++ HomeAutomation.Website/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HomeAutomation.Website.Pages;
+
+public class IndexModel(ILogger<IndexModel> logger) : PageModel
+{
+	public void OnGet()
+	{
+
+	}
+}
