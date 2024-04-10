caa9a2e072de5b045e9b32912b3fcf8c2adae549
bob h <mail@bob.codes>
2024-04-10 21:38:23 +0100

    tidying configs

--- build.ps1
+++ build.ps1
@@ -3,22 +3,15 @@ docker pull mcr.microsoft.com/dotnet/aspnet:8.0 || exit 1
 docker pull mcr.microsoft.com/dotnet/sdk:8.0 || exit 1
 
 # build
-$secret = 'id=ca_crt,src={0}\.aspnet\https\ca.crt' -f ${env:userprofile}
 docker build `
-	--build-arg "NuGetServerApiKey=${env:NuGetServerApiKey}" `
-	--file .\HomeAutomation.Website\Dockerfile `
-	--no-cache `
-	--secret $secret `
-	--tag eassbhhtgu/homeautomation-website:latest `
+	--file '.\HomeAutomation.Website\Dockerfile' `
+	--secret "id=ca_crt,src=${env:userprofile}\.aspnet\https\ca.crt" `
+	--tag 'eassbhhtgu/homeautomation-website:latest' `
 	.
 
 # build
-$secret = 'id=ca_crt,src={0}\.aspnet\https\ca.crt' -f ${env:userprofile}
 docker build `
-	--build-arg "NuGetServerApiKey=${env:NuGetServerApiKey}" `
-	--file .\HomeAutomation.WebApplication\Dockerfile `
-	--no-cache `
-	--secret $secret `
-	--tag eassbhhtgu/homeautomation-api:latest `
+	--file '.\HomeAutomation.WebApplication\Dockerfile' `
+	--secret "id=ca_crt,src=${env:userprofile}\.aspnet\https\ca.crt" `
+	--tag 'eassbhhtgu/homeautomation-api:latest' `
 	.
-
